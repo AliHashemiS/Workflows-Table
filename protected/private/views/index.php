adf625936022a17ce9217b1eca56b64b63dfe7d8
AliHashemiS <79598781+AliHashemiS@users.noreply.github.com>
2021-12-28 00:27:52 -0600

    Created login and register interface UI
    php and css created

---
+++ protected/private/views/index.php
@@ -0,0 +1,46 @@
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="Expires" content="0">
+    <meta http-equiv="Last-Modified" content="0">
+    <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
+    <meta http-equiv="Pragma" content="no-cache">
+    <link rel="stylesheet" href="/assets/css/login.css">
+    <title>SignIn</title>
+</head>
+<body>
+    <div class="divPrincipal">
+        <table class="principalTable" align="center" cellpadding="10" cellspacing="20">
+            <tr>  
+                <td>   
+                    <form>
+                        <h1 style="margin-bottom: 10%;">Sign In</h1>     
+
+                        <div class="divInputs"> 
+                            <input type="email" id="inputEmail" class="form-control" placeholder="Email address" required="" autofocus="">
+                        </div>
+
+                        <div class="divInputs">
+                            <input type="password" id="inputPassword" class="form-control" placeholder="Password" required="">
+                        </div>
+                        
+                        <div class="divInputs">
+                            <button class="button" type="submit">Sign In</button>
+                        </div>
+                        <div class="divInputs">
+                            <a href='Register.php'>Sing Up</a>
+                        </div>
+                    </form>
+                </td>
+            </tr>   
+        </table>
+    </div>
+</body>
+</html>
+
+<?php
+
+
+?> 
