aef87fd9f1b850da66f9386edfb67272d34fbcbe
AliHashemiS <79598781+AliHashemiS@users.noreply.github.com>
2022-01-06 12:16:26 -0600

    Created Class Register

--- app/controllers/register.class.php
+++ app/controllers/register.class.php
@@ -2,13 +2,47 @@
 
 	class Register extends Controller {
 
+		private $userModel;
+
 		public function __construct() {
-		
+			$this->userModel = $this->loadModel('user');
 		}
-		
 		
 		public function index() {
 			$this->render('register');
 		}
 
+		public function register() {
+
+			$data = [
+                'userEmail' => '',
+                'userPass' => '',
+            ];
+
+			if($_SERVER['REQUEST_METHOD'] == 'POST'){	
+				if(isset($_POST['userEmail']) && isset($_POST['userPass'])){
+
+					$data = [
+						'userEmail' => trim($_POST['userEmail']),
+						'userPass' => trim($_POST['userPass']),
+					];
+					
+					if($this->userModel->findUserbyEmail($data['userEmail'])){
+						$result = [
+							'success' => false,
+							'message' => "El email ya existe"
+						];
+						return $this->sendResponse($result,404);
+                    }else{
+						$object = $this->userModel->register($data);
+						$result = [
+							'success' => true,
+							'data' => $object,
+							'message' => "El Usuario ".$data['userEmail']." ha sido creado correctamente!"
+						];
+						return $this->sendResponse($result,200);
+					}
+				}
+			}
+		}
 	}
