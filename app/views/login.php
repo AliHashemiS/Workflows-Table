e507512c41c89fe20d67deaa5dad830f8f9a8d95
AliHashemiS <79598781+AliHashemiS@users.noreply.github.com>
2022-01-06 12:17:45 -0600

    Created view login

--- app/views/login.php
+++ app/views/login.php
@@ -7,23 +7,27 @@
     <meta http-equiv="Last-Modified" content="0">
     <meta http-equiv="Cache-Control" content="no-cache, mustrevalidate">
     <meta http-equiv="Pragma" content="no-cache">
-    <link rel="stylesheet" href="./assets/css/login.style.css">
+    <link rel="stylesheet" href="/assets/css/login.style.css">
+    <link rel="stylesheet" href="/assets/css/style.css">
+    <script src="/assets/js/request.js"></script>
+    <script src="/assets/js/user.js"></script>
     <title>SignIn</title>
 </head>
 <body>
+    <div id="alert" class=""></div>
     <div class="divPrincipal">
         <table class="principalTable" align="center" cellpadding="10" cellspacing="20">
             <tr>  
                 <td>   
-                    <form>
+                    <form id="login" onsubmit="loginUser(event)">
                         <h1 style="margin-bottom: 10%;">Sign In</h1>     
 
                         <div class="divInputs"> 
-                            <input type="email" id="inputEmail" placeholder="Email address" required="" autofocus="">
+                            <input type="email" name="inputEmail" placeholder="Email address" required="" autofocus="">
                         </div>
 
                         <div class="divInputs">
-                            <input type="password" id="inputPassword" placeholder="Password" required="" autofocus="">
+                            <input type="password" name="inputPassword" placeholder="Password" required="" autofocus="">
                         </div>
                         
                         <div class="divInputs">
